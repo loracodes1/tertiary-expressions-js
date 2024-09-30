44fb40ab746e936013e1a10dfae4880644d64528
florence <loracodes3@gmail.com>
2024-09-30 17:49:01 +0300

    assign appropriate greeting according to time

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-// Ternary expression is a shortcut to if {} and else{} statements.
+// Ternary operator is a shortcut to if {} and else{} statements.
 //Helps to assign a variable based on a condition!
 //FORMAT => condition ? codeIfTrue : codeIfFalse
 
+let age =21;
+let message =age >=18 ? "You are an adult" : 'You are a minor';
+console.log(message)
+
+let time =16;
+let greeting=time < 12 ?'Good Morning' : 'Good Afternoon';
+console.log(greeting)
